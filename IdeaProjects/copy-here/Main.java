83a386f6e2e2afb9a269b6d98cdebbe34afd84a6
agalilo22 <125953553+agalilo22@users.noreply.github.com>
2024-09-24 13:59:50 +0800

    intial commit

---
+++ IdeaProjects/copy-here/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("My name is Abigail Galilo!");
+        System.out.println("I am practicing my programming skills.");
+        ;
+
+    }
+}
